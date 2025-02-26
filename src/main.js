d9478e527c2ae7ca2b93bbd21d57c739df7b8734
Adrien Richard <you@example.com>
2025-02-26 09:53:07 +0100

    shadows

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('three-canvas') });
 renderer.setSize(900, 600);
 renderer.setAnimationLoop(animate);
+renderer.shadowMap.enabled = true;
 
 const scene = new THREE.Scene();
 
@@ -14,7 +15,7 @@ const camera = new THREE.PerspectiveCamera(70, 900 / 600);
 camera.position.z = 5;
 camera.position.x = 2;
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const _controls = new OrbitControls(camera, renderer.domElement);
 
 
 /********/
@@ -23,45 +24,53 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const textureLoader = new THREE.TextureLoader();
 
 // Earth
+const earthRotationRate = 0.6;
+
 const earth = new THREE.Mesh(
     new THREE.SphereGeometry(1),
     new THREE.MeshStandardMaterial({ map: textureLoader.load('assets/textures/earth.jpg') }),
 )
 scene.add(earth);
 
-const earthRotationRate = 0.6;
-
+earth.castShadow = true;
+earth.receiveShadow = true;
 
 // Moon
+const moonSpeed = 1;
+const moonDistanceFromEarth = 2;
+
 const moon = new THREE.Mesh(
     new THREE.TorusGeometry(0.2, 0.08),
     new THREE.MeshStandardMaterial({ map: textureLoader.load('assets/textures/moon.jpg') }),
 )
 scene.add(moon);
 
-const moonSpeed = 1;
-const moonDistanceFromEarth = 2;
-
 moon.position.x = moonDistanceFromEarth;
+moon.castShadow = true;
+moon.receiveShadow = true;
 
 // Sun
 const sun = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
     new THREE.MeshBasicMaterial({ color: new THREE.Color('lightyellow') }),
 )
-sun.position.x = 6;
 scene.add(sun);
+
+sun.position.x = 6;
 
 /*******/
 /*Lights*/
 /*******/
+// Ambient light
 const ambientLight = new THREE.AmbientLight(new THREE.Color('white'), 0.1);
 scene.add(ambientLight);
 
+// Directional Light
 const directionalLight = new THREE.DirectionalLight(new THREE.Color('white'));
-directionalLight.position.x = 6;
 scene.add(directionalLight);
 
+directionalLight.position.x = 6;
+directionalLight.castShadow = true;
 
 /***********/
 /*Animation*/
