2f54db73479b88668f69b9a0f547e983c04e8ce0
Adrien Richard <you@example.com>
2025-02-26 09:12:23 +0100

    shaded meshes and lights

--- src/main.js
+++ src/main.js
@@ -17,22 +17,34 @@ camera.position.x = 2;
 /********/
 const earth = new THREE.Mesh(
     new THREE.SphereGeometry(1),
-    new THREE.MeshBasicMaterial(new THREE.Color('white')),
+    new THREE.MeshStandardMaterial({ color: new THREE.Color('white') }),
 )
 scene.add(earth);
 
 const moon = new THREE.Mesh(
     new THREE.TorusGeometry(0.1, 0.04),
-    new THREE.MeshBasicMaterial(new THREE.Color('white')),
+    new THREE.MeshStandardMaterial({ color: new THREE.Color('white') }),
 )
 moon.position.x = 2;
 scene.add(moon);
 
 const sun = new THREE.Mesh(
     new THREE.BoxGeometry(3, 3, 3),
-    new THREE.MeshBasicMaterial(new THREE.Color('white')),
+    new THREE.MeshBasicMaterial({ color: new THREE.Color('lightyellow') }),
 )
 sun.position.x = 6;
 scene.add(sun);
+
+/*******/
+/*Lights*/
+/*******/
+const ambientLight = new THREE.AmbientLight(new THREE.Color('white'), 0.1);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(new THREE.Color('white'));
+directionalLight.position.x = 6;
+scene.add(directionalLight);
+
+
 
 renderer.render(scene, camera);
